df32bb08684621703d975ef7461d4948e72cd763
Hiroaki Goto <goto.inct@gmail.com>
2017-11-12 10:31:45 +0900

    Add copyright

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//
+// Project Name: server_monitor
+// File Name: main.rs
+// Last modified: 2017/11/12
+// Author: Hiroaki Goto
+//
+// Copyright (c) 2017 Hiroaki Goto. All rights reserved.
+//
+
 fn main() {
     println!("Hello, world!");
 }
